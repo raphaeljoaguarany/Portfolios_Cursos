7d2cb109b4bf39792befd717ddf2957d0d2107b5
Raphael J <126215131+raphaeljoaguarany@users.noreply.github.com>
2023-12-06 18:05:28 -0300

    Add files via upload

---
+++ Curso java/Screenmatch/src/br/com/alura/screenmatch/calculos/CalculadoraDeTempo.java
@@ -0,0 +1,24 @@
+package br.com.alura.screenmatch.calculos;
+
+import br.com.alura.screenmatch.modelos.Titulo;
+
+public class CalculadoraDeTempo {
+    private int tempoTotal = 0;
+
+    public int getTempoTotal() {
+        return this.tempoTotal;
+    }
+
+//    public void inclui(Filme f){
+//        this.tempoTotal += f.getDuracaoEmMinutos();
+//    }
+//
+//    public void inclui(Serie f){
+//        this.tempoTotal += f.getDuracaoEmMinutos();
+//    }
+
+    public void inclui(Titulo titulo){
+        System.out.println("Adicionando duração em minutos de " + titulo);
+        this.tempoTotal += titulo.getDuracaoEmMinutos();
+    }
+}
